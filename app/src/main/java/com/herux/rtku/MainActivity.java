332788efc41b09cdd5073012f940f8fae112fd85
Heru Susanto <herux@amg.co.id>
2019-12-18 15:42:09 +0700

    update login layout

--- app/src/main/java/com/herux/rtku/MainActivity.java
+++ app/src/main/java/com/herux/rtku/MainActivity.java
@@ -2,13 +2,26 @@ package com.herux.rtku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }else{
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
         setContentView(R.layout.activity_main);
     }
 }
